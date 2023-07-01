3be9088cab2071054903cf869fb72288c213e1a9
Albert Jankowski <ajankowski@q-centrix.com>
2023-07-01 10:51:50 -0500

    Initial commit

---
+++ spec/cloudflare/email_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Cloudflare::Email do
+  it "has a version number" do
+    expect(Cloudflare::Email::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
