0fb2955d1cec061279a3f55e7a19bc8638dd95a8
Albert Jankowski <ajankowski@q-centrix.com>
2023-07-01 11:09:19 -0500

    Fix spec

--- spec/cloudflare/email_spec.rb
+++ spec/cloudflare/email_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Cloudflare::Email do
   it "has a version number" do
     expect(Cloudflare::Email::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
